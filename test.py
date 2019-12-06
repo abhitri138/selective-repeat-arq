e20d51a0110f8aa1bda8c1701dae97ebdf968e4d
Abhishek Tripathi <abhishektripathi138@gmail.com>
2019-12-05 21:46:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,36 +7,39 @@ if __name__ == "__main__":
     server_host = args[0]
     server_port = args[1]
     file_name = args[2]
-    N = [256, 512, 1024]
+    N = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
     MSS = []
     N_times = []
     MSS_times = []
 
-    # for i in N:
-    #     avg = 0
-    #     for j in range(4):
-    #         avg += sftp_client.main(server_host, server_port, file_name, i, 500)
-    #         time.sleep(40)
-    #     avg /= 4
-    #     N_times.append(avg)
-    #     print("Window Size: ", i, " time: ", avg * 10)
-    avg = 0
-    for j in range(4):
-        avg += sftp_client.main(server_host, server_port, file_name, 64, 500)
-        time.sleep(40)
-    avg /= 4
-    N_times.append(avg)
-    print(" time: ", avg * 10)
+    for i in N:
+        avg = 0
+        for j in range(4):
+            avg += sftp_client.main(server_host, server_port, file_name, i, 500)
+            time.sleep(40)
+        avg /= 4
+        N_times.append(avg)
+        print("Window Size: ", i, " time: ", avg * 10)
+        
+        
+#     avg = 0
+#     for j in range(4):
+#         avg += sftp_client.main(server_host, server_port, file_name, 64, 500)
+#         time.sleep(40)
+#     avg /= 4
+#     N_times.append(avg)
+#     print(" time: ", avg * 10)
     # client.start(64, 900)
+    
 
-    # for i in range(200,300, 100):
-    #     MSS.append(i)
-    #     avg = 0
-    #     for j in range(3):
-    #         avg += sftp_client.main(server_host, server_port, file_name, 64, i)
-    #         time.sleep(40)
-    #     avg /= 3
-    #     MSS_times.append(avg)
-    #     print("MSS: ", i, " time: ", avg * 10)
+    for i in range(100,1100, 100):
+        MSS.append(i)
+        avg = 0
+        for j in range(3):
+            avg += sftp_client.main(server_host, server_port, file_name, 64, i)
+            time.sleep(40)
+        avg /= 3
+        MSS_times.append(avg)
+        print("MSS: ", i, " time: ", avg * 10)
     print(N_times)
-    # print(MSS_times)
+    print(MSS_times)
